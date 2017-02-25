6ef6c601f753cc96a9ee83ea8ed2d4a875e900fd
Akassh Shah <akassh.shah77@gmail.com>
2017-02-25 02:42:58 -0500

    Modified java file

---
+++ CardShuffler.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class CardShuffler{
+   public static String[] genUnshuffledCards(){
+      String[] cardNumbers={"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+      String[] cardSuites={"Spades","Clubs","Hearts","Diamonds"};
+      String[] unshuffledDeck= new String[52];
+      int k=0;
+      for(int i=0;i<cardNumbers.length;i++){
+         for(int j=0;j<cardSuites.length;j++){
+            
+         }
+      }
+   }
+}
